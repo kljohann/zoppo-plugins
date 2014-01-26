4d7c9b8e2ab1aefd1c02490842c574e2015b9c99
Johann Klähn <kljohann@gmail.com>
2014-01-26 10:50:12 +0100

    add 'eg' command

--- utility/init.zsh
+++ utility/init.zsh
@@ -252,4 +252,38 @@ if zdefault -t ':zoppo:plugin:utility:fork' enable 'yes'; then
 fi
 # }}}
 
+# Documentation Lookup and Man Pages {{{
+function eg() {
+  (( $+1 )) || {
+    cat 1>&2 <<END
+Usage: $0 [<args>...] [<expr>]
+
+  args: Arguments to pass to man.
+  expr: Command line argument to search for.
+        Defaults to jump to the "EXAMPLE" section.
+
+Examples:
+
+  $0 find
+  $0 less -s
+  $0 1 less -s
+  $0 zshall HIST_IGNORE_SPACE
+  $0 zshall fc
+END
+    return 1
+  }
+
+  local expr='^EXAMPLE'
+  local -a args
+  args=("$@")
+
+  (( $+2 )) && {
+    expr="^[[:blank:]]*${@[-1]}"
+    args=("${@[1,-2]}")
+  }
+
+  PAGER="less -g -s '+/$expr'" man "${=args}"
+}
+# }}}
+
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
